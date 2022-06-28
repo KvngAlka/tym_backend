6822cb9e79ce0bd469915d913c000b9fbabe508a
KvngAlka <kingsford499@gmail.com>
2022-06-28 23:21:43 +0000

    modified server.js

--- server.js
+++ server.js
@@ -54,4 +54,4 @@ app.get("/cdrs", (_,res)=>{
 
 
 
-app.listen(port,()=> console.log(`Server running on http://localhost:${port}`))
+app.listen(port,'0.0.0.0',()=> console.log(`Server running on http://0.0.0.0:${port}`))
